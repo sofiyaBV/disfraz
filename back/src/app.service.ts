02f8719add3f2d1c7df8673ca86ddf867c5e52f3
Den4562 <119700990+Den4562@users.noreply.github.com>
2025-01-18 17:07:02 +0200

    Connection to DataBase

--- back/src/app.service.ts
+++ back/src/app.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { DataSource } from 'typeorm';
 
 @Injectable()
 export class AppService {
+  constructor(private dataSource: DataSource) {}
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async checkDatabaseConnection(): Promise<string> {
+    try {
+      await this.dataSource.initialize(); // Инициализируем подключение
+      return 'Подключение к базе данных успешно!';
+    } catch (error) {
+      console.error('Ошибка подключения к базе данных:', error);
+      return 'Ошибка подключения к базе данных.';
+    }
   }
 }
