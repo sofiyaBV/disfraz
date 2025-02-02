aa6958c769dc64056c5a4e66b10983923c21adbe
Den4562 <119700990+Den4562@users.noreply.github.com>
2025-02-02 15:26:44 +0200

    Integration Swagger

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Конфигурация Swagger
+  const config = new DocumentBuilder()
+    .setTitle('API Documentation') // Название API
+    .setDescription('API для вашего проекта') // Описание
+    .setVersion('1.0') // Версия API
+    // .addBearerAuth() 
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
