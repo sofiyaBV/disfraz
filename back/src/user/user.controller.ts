5bbb89d3dc168b943e9555b58c490c3a441018f0
Den4562 <119700990+Den4562@users.noreply.github.com>
2025-02-02 15:26:44 +0200

    Integration Swagger

--- back/src/user/user.controller.ts
+++ back/src/user/user.controller.ts
@@ -1,27 +1,34 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './user.entity';
+import { ApiTags, ApiOperation, ApiResponse, ApiBody } from '@nestjs/swagger';
+import { CreateUserDto } from '../dto/create-user.dto';
 
+@ApiTags('users') // Группировка эндпоинтов в Swagger
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Создать пользователя' })
+  @ApiResponse({ status: 201, description: 'Пользователь успешно создан' })
+  @ApiBody({ type: CreateUserDto }) // Подключаем DTO, чтобы Swagger знал, какие параметры ожидать
   create(@Body() user: Partial<User>) {
     return this.userService.create(user);
   }
 
-  @Post('create')
-  async createUser(@Body() userData: { username: string; password: string }) {
-    return this.userService.create(userData);
-  }
 
   @Get()
+  @ApiOperation({ summary: 'Получить всех пользователей' })
+  @ApiResponse({ status: 200, description: 'Список пользователей', type: [User] })
   findAll() {
     return this.userService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Получить пользователя по ID' })
+  @ApiResponse({ status: 200, description: 'Найденный пользователь', type: User })
+  @ApiResponse({ status: 404, description: 'Пользователь не найден' })
   findOne(@Param('id') id: number) {
     return this.userService.findOne(id);
   }
