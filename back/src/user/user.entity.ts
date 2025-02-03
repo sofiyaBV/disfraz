d37386f2f202ec5cba8873e28af6555450fec1c1
Den4562 <119700990+Den4562@users.noreply.github.com>
2025-02-03 16:54:32 +0200

    Update

--- back/src/user/user.entity.ts
+++ back/src/user/user.entity.ts
@@ -1,7 +1,7 @@
 import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
 import { IsNotEmpty, MinLength } from 'class-validator';
 
-@Entity()
+@Entity('user')
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
