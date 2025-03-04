d51f9eb6cbf88239d2a13e6f55d6b3fa9fe3cf4e
Бондар Софія <cofiyabondar8@gmail.com>
2025-03-04 16:08:44 +0200

    front-admin start

---
+++ front-admin/src/pages/users/users.tsx
@@ -0,0 +1,53 @@
+import { Create, Datagrid, Edit, EditButton, EmailField, List, ReferenceInput, Show, SimpleForm, TextField, TextInput } from "react-admin";
+
+const userFilters = [
+    <TextInput source="q" label="Search" alwaysOn />,//Поле для поиска
+    // <ReferenceInput source="userId" label="User" reference="users" />,
+]
+
+export const UserList = () => (
+    <List filters={userFilters}>
+        <Datagrid rowClick = "show">
+            <TextField source="id" />
+            <TextField source="name" />
+            <TextField source="username" />
+            <EmailField source="email" />
+            <TextField source="phone" />
+            <EditButton />
+        </Datagrid>
+    </List>
+);
+
+export const UserEdit = () => (
+    <Edit>
+        <SimpleForm>
+            <TextInput source="name" />
+            <TextInput source="username" />
+            <TextInput source="email" />
+            <TextInput source="phone" />
+        </SimpleForm>
+    </Edit>
+);
+
+export const UserShow = () => (
+    <Show>
+        <SimpleForm>
+            <TextField source="id" />
+            <TextField source="name" />
+            <TextField source="username" />
+            <EmailField source="email" />
+            <TextField source="phone" />
+        </SimpleForm>
+    </Show>
+);
+
+export const UserCreate = () => (
+    <Create>
+        <SimpleForm>
+            <TextInput source="name" />
+            <TextInput source="username" />
+            <TextInput source="email" />
+            <TextInput source="phone" />
+        </SimpleForm>
+    </Create>
+);
