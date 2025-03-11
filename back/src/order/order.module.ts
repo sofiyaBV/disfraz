830a6b856f9ca8e771951e0dec24997cdbd8383a
Den4562 <119700990+Den4562@users.noreply.github.com>
2025-03-11 19:52:52 +0200

    Create a relationship between Cart and Order tables

--- back/src/order/order.module.ts
+++ back/src/order/order.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
 import { Order } from './entities/order.entity';
+import { Cart } from 'src/cart/entities/cart.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Order])], // Подключаем модель Order
+  imports: [TypeOrmModule.forFeature([Order, Cart])], // Подключаем модель Order
   controllers: [OrderController],
   providers: [OrderService],
   exports: [OrderService], // Для использования в других модулях
