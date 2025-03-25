5acf864b7655e4680380ec4920c3c3cacd3a2134
Den4562 <119700990+Den4562@users.noreply.github.com>
2025-03-25 07:35:11 +0200

    Added pagination in product

--- back/src/config/data-source.ts
+++ back/src/config/data-source.ts
@@ -16,7 +16,7 @@ export const AppDataSource = new DataSource({
   host: process.env.POSTGRES_HOST,
   port: parseInt(process.env.POSTGRES_PORT, 10) || 5432,
   username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD, // Переконайтеся, що це рядок
+  password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
   entities: [
     Product,
