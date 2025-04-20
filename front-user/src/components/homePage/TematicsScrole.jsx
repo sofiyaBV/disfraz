17652e208762a45ad55883afe6e68f49c1332a33
Бондар Софія <cofiyabondar8@gmail.com>
2025-04-21 00:50:29 +0300

    tematicScrole

---
+++ front-user/src/components/homePage/TematicsScrole.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import ButtonGeneral from "../buttons/ButtonGeneral";
+import style from "../../style/tematicsScrole.module.css";
+
+const TematicsScrole = ({ text, bacgraundImg, buttonLink }) => {
+  // Извлекаем URL из импортированного изображения
+  const imageUrl =
+    typeof bacgraundImg === "object" && bacgraundImg.default
+      ? bacgraundImg.default
+      : bacgraundImg;
+  const upperCaseText = text.toUpperCase();
+  return (
+    <div
+      className={style.container}
+      style={{ backgroundImage: `url(${imageUrl})` }}
+    >
+      <div className={style.content}>
+        <h1>{upperCaseText}</h1>
+        <ButtonGeneral
+          initialColor="transparent"
+          borderColor="white"
+          text="Дивитись"
+          width="clamp(1rem, 11vw, 49rem)"
+          height="clamp(2rem, 5vw, 4.13rem)"
+          link={buttonLink}
+          textColor="white"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default TematicsScrole;
--- front-user/src/pages/HomePage.js
+++ front-user/src/pages/HomePage.js
@@ -5,6 +5,8 @@ import newsData from "../utils/NewsData";
 import offersData from "../utils/OffersData";
 import SuitWM from "../components/homePage/SuitWM";
 import NewsOnTheSite from "../components/homePage/NewsOnTheSite";
+import TematicsScrole from "../components/homePage/TematicsScrole";
+import TematicsData from "../utils/TematicsData"; // Импортируем данные
 
 import img1man from "../img/newsS/man1.png";
 import img2man from "../img/newsS/man2.png";
@@ -13,7 +15,9 @@ import img2woman from "../img/newsS/women2.png";
 
 const HomePage = () => {
   const scrollRef = useRef(null);
-  const [currentNewsIndex, setCurrentNewsIndex] = useState(0); // Состояние для текущей новости
+  const tematicsScrollRef = useRef(null); // Новый реф для TematicsScrole
+  const [currentNewsIndex, setCurrentNewsIndex] = useState(0);
+  const [currentTematicsIndex, setCurrentTematicsIndex] = useState(0); // Индекс для TematicsScrole
 
   // Автопрокрутка для Offers
   useEffect(() => {
@@ -44,12 +48,37 @@ const HomePage = () => {
       setCurrentNewsIndex((prevIndex) =>
         prevIndex === newsData.length - 1 ? 0 : prevIndex + 1
       );
-    }, 5000); // Смена каждые 5 секунд
+    }, 5000);
 
     return () => clearInterval(newsInterval);
   }, []);
 
-  const currentNews = newsData[currentNewsIndex]; // Текущая новость
+  // Автопрокрутка для TematicsScrole (по 2 карточки)
+  useEffect(() => {
+    const tematicsContainer = tematicsScrollRef.current;
+    if (!tematicsContainer) return;
+
+    const cardWidth = tematicsContainer.scrollWidth / (TematicsData.length / 2); // Ширина двух карточек
+    let scrollPosition = 0;
+
+    const scrollInterval = setInterval(() => {
+      scrollPosition += cardWidth;
+      if (scrollPosition >= tematicsContainer.scrollWidth) {
+        scrollPosition = 0;
+      }
+      tematicsContainer.scrollTo({
+        left: scrollPosition,
+        behavior: "smooth",
+      });
+      setCurrentTematicsIndex((prevIndex) =>
+        prevIndex + 2 >= TematicsData.length ? 0 : prevIndex + 2
+      );
+    }, 5000);
+
+    return () => clearInterval(scrollInterval);
+  }, []);
+
+  const currentNews = newsData[currentNewsIndex];
 
   return (
     <div className={style.general}>
@@ -74,7 +103,7 @@ const HomePage = () => {
             productName={currentNews.productName}
             img={currentNews.img}
             link={currentNews.link}
-            newsKey={currentNewsIndex} // Передаем индекс для анимации
+            newsKey={currentNewsIndex}
           />
         </div>
         <div className={style.news_general}>
@@ -83,6 +112,21 @@ const HomePage = () => {
         <div className={style.news_general}>
           <SuitWM title="Жіночі костюми" img1={img1woman} img2={img2woman} />
         </div>
+      </div>
+      {/* Скролл TematicsScrole */}
+      <div className={style.scrol_tematic} ref={tematicsScrollRef}>
+        {TematicsData.map((item, index) => (
+          <TematicsScrole
+            text={item.title}
+            bacgraundImg={item.img}
+            buttonLink={item.link}
+          />
+        ))}
+      </div>
+      {/* Scrol Categories */}
+      <div>
+        <h3>ПОПУЛЯРНІ КАТЕГОРІЇ</h3>
+        <div ref={tematicsScrollRef}></div>
       </div>
     </div>
   );
